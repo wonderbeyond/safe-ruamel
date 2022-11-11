88c9e73eb895a2d9d6010269dddd4ab8390ae1d0
Wanchao Zhang <wanchao.zhang@xtalpi.com>
2022-11-11 18:08:57 +0800

    compat

--- safe_ruamel.py
+++ safe_ruamel.py
@@ -1,7 +1,7 @@
 """
 This module is published at https://gist.github.com/wonderbeyond/f6c24690db42c8787cd8feabe1dd91d5
 """
-from typing import cast, IO
+from typing import cast, IO, Union
 import io
 import os
 from concurrent.futures import ProcessPoolExecutor
@@ -16,13 +16,15 @@ _x_process_executor = ProcessPoolExecutor(max_workers=os.cpu_count())
 
 
 def _yaml_load(input: str):
-    if not (yaml := _process_cache.get('yaml')):
+    yaml = _process_cache.get('yaml')
+    if not yaml:
         yaml = _process_cache['yaml'] = ruamel.yaml.YAML()
     return yaml.load(input)
 
 
 def _yaml_dump(data) -> str:
-    if not (yaml := _process_cache.get('yaml')):
+    yaml = _process_cache.get('yaml')
+    if not yaml:
         yaml = _process_cache['yaml'] = ruamel.yaml.YAML()
     buf = io.StringIO()
     yaml.dump(data, buf)
@@ -32,12 +34,12 @@ def _yaml_dump(data) -> str:
 
 class YAML:
     """A proxy class for ruamel.yaml.YAML that bypass the thread safety issue."""
-    def load(self, raw: str | bytes | IO, /):
+    def load(self, raw: Union[str, bytes, IO]):
         raw = cast(IO, raw).read() if isinstance(raw, io.IOBase) else raw
-        fut = _x_process_executor.submit(_yaml_load, raw)
+        fut = _x_process_executor.submit(_yaml_load, cast(str, raw))
         return fut.result()
 
-    def dump(self, data, stream: IO = None) -> IO:
+    def dump(self, data, stream: Union[IO, None] = None) -> IO:
         stream = io.StringIO() if stream is None else stream
         fut = _x_process_executor.submit(_yaml_dump, data)
         out = fut.result()
